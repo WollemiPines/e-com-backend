8d6b633b017090d6283d8f5536224c3da9482f00
Chris Wallace <Chrisw1096@gmail.com>
2022-08-15 08:32:29 +1000

    insomnia returning empty arr

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
     const productData = await Product.findAll();
     res.status(200).json(productData);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err);s
   }
 });
 
