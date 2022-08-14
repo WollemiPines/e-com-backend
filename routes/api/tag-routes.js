132f29203e688de508797ce13d83b59e736ecbe1
Chris Wallace <Chrisw1096@gmail.com>
2022-08-14 14:37:42 +1000

    pushing for review

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,12 +41,33 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',async  (req, res) => {
   // update a tag's name by its `id` value
+  try {
+  const selId= await selId.findOne({ where: { tag_id: req.body.tag_id } });
+  selId.name = req.body.name 
+  await selId.save()
+res.status(200).json(selId);
+} catch (err) {
+  res.status(400).json(err);
+}
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+
+ // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagsData = await Tag.destroy({
+      where: { id: req.params.id }
+    });
+    if (!tagsData) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
